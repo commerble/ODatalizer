7c71fa983f9aaf19158d7fbfd995758d78520652
yoshiyuki taniguchi <taniguchi@commerble.com>
2020-06-18 14:49:54 +0900

    modify metadata test

--- src/ODatalizer.EFCore.Tests/Host/ODatalizerWebApplicationFactory.cs
+++ src/ODatalizer.EFCore.Tests/Host/ODatalizerWebApplicationFactory.cs
@@ -1,49 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Sample.EFCore;
-using Sample.EFCore.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace ODatalizer.EFCore.Tests.Host
 {
-    public class ODatalizerWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+    public class ODatalizerWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices(services =>
-            {
-                // Create a new service provider.
-                var serviceProvider = new ServiceCollection()
-                    .AddEntityFrameworkInMemoryDatabase()
-                    .BuildServiceProvider();
-
-                // Add a database context using an in-memory database for testing.
-                services.AddDbContext<SampleDbContext>(options =>
-                {
-                    options.UseInMemoryDatabase("e2etest");
-                    options.UseInternalServiceProvider(serviceProvider);
-                });
-
-                // Build the service provider.
-                var sp = services.BuildServiceProvider();
-
-                // Create a scope to obtain a reference to the database contexts
-                using (var scope = sp.CreateScope())
-                {
-                    var scopedServices = scope.ServiceProvider;
-                    var sample = scopedServices.GetRequiredService<SampleDbContext>();
-
-                    var logger = scopedServices.GetRequiredService<ILogger<ODatalizerWebApplicationFactory<TStartup>>>();
-
-                    // Ensure the database is created.
-                    sample.Database.EnsureCreated();
-                }
-            });
-        }
     }
 }
--- src/ODatalizer.EFCore.Tests/MetadataTest.cs
+++ src/ODatalizer.EFCore.Tests/MetadataTest.cs
@@ -1,7 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
 using Newtonsoft.Json.Linq;
+using ODatalizer.EFCore.Builders;
 using ODatalizer.EFCore.Tests.Host;
 using Sample.EFCore;
-using System;
+using Sample.EFCore.Data;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,13 +12,17 @@ using Xunit;
 
 namespace ODatalizer.EFCore.Tests
 {
-    public class BasicTest : IClassFixture<ODatalizerWebApplicationFactory<Startup>>
+    public class MetadataTest : IClassFixture<ODatalizerWebApplicationFactory<Startup>>
     {
         private readonly HttpClient _client;
+        private readonly IEdmModel _edm;
 
-        public BasicTest(ODatalizerWebApplicationFactory<Startup> factory)
+        public MetadataTest(ODatalizerWebApplicationFactory<Startup> factory)
         {
             _client = factory.CreateClient();
+
+            using var scope = factory.Server.Services.CreateScope();
+            _edm = EdmBuilder.Build(scope.ServiceProvider.GetRequiredService<SampleDbContext>());
         }
 
         [Fact]
@@ -28,9 +35,17 @@ namespace ODatalizer.EFCore.Tests
             var result = JObject.Parse(await response.Content.ReadAsStringAsync());
 
             var actual = result.SelectTokens("$.value[:].name").Select(o => (string)o).OrderBy(o => o);
-            var expected = new[] { "Campaigns", "CampaignActions", "Categories", "Products", "SalesPatterns", "SalesProducts" }.OrderBy(o => o);
+            var expected = _edm.EntityContainer.EntitySets().Select(set => set.Name).OrderBy(o => o);
 
             Assert.True(Enumerable.SequenceEqual(expected, actual));
+        }
+
+        [Fact]
+        public async Task GetMetadata()
+        {
+            var response = await _client.GetAsync("/sample/$metadata");
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal("application/xml", response.Content.Headers.ContentType.MediaType);
         }
     }
 }
