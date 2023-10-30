fc1c5e061fdc43c25b904b95f531a7fd6275dd5a
yoshiyuki taniguchi <taniguchi@commerble.com>
2023-10-30 16:55:38 +0900

    modify EdmBulder
    override NullableProperty value settings on model creating

--- src/ODatalizer.EFCore/Builders/EdmBuilder.cs
+++ src/ODatalizer.EFCore/Builders/EdmBuilder.cs
@@ -2,7 +2,10 @@
 using Microsoft.OData.Edm;
 using Microsoft.OData.ModelBuilder;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Intrinsics.Arm;
 
 namespace ODatalizer.EFCore.Builders
 {
@@ -40,7 +43,52 @@ namespace ODatalizer.EFCore.Builders
                 builder.AddEntitySet(dbSet.Name, type);
             }
 
+
+            // ODataConventionModelBuilder make ForeignKey be Nullable=True even if the column type of RDB is NOT NULL.
+            // Save original column Nullable settings and restore it on model creating.
+            builder.OnModelCreating = builder =>
+            {
+                foreach (var st in builder.StructuralTypes)
+                {
+                    var et = st as EntityTypeConfiguration;
+                    foreach (var nav in st.NavigationProperties)
+                    {
+                        foreach (var dp in nav.DependentProperties)
+                        {
+                            if (IsNotNull(et, dp))
+                            {
+                                var prop = Find(st, dp);
+                                if (prop != null)
+                                {
+                                    prop.NullableProperty = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
             return builder.GetEdmModel();
+        }
+
+        private static readonly Type _required = typeof(RequiredAttribute);
+        private static bool IsNotNull(EntityTypeConfiguration? entityTypeConfiguration, PropertyInfo info)
+        {
+            if (entityTypeConfiguration?.Keys.Any(key => key.PropertyInfo == info) == true)
+                return true;
+
+            if (info.PropertyType.IsPrimitive)
+                return true;
+
+            if (info.CustomAttributes.Any(attr => attr.AttributeType == _required))
+                return true;
+
+            return false;
+        }
+        private static PrimitivePropertyConfiguration Find(StructuralTypeConfiguration st, PropertyInfo info)
+        {
+            return st.Properties.FirstOrDefault(p => p.PropertyInfo == info) as PrimitivePropertyConfiguration;
         }
     }
 }
--- src/Sample.EFCore/Data/AnotherDbContext.cs
+++ src/Sample.EFCore/Data/AnotherDbContext.cs
@@ -1,14 +1,60 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sample.EFCore.AnotherEntities;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Sample.EFCore.Data
 {
     public class AnotherDbContext : DbContext
     {
-        public DbSet<Product> Products { get; set; }
+        public DbSet<A> As { get; set; }
+        public DbSet<B> Bs { get; set; }
+        public DbSet<C> Cs { get; set; }
+        public DbSet<D> Ds { get; set; }
+        public DbSet<E> Es { get; set; }
         public AnotherDbContext([NotNull] DbContextOptions<AnotherDbContext> options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<D>().HasKey(o => new { o.Id, o.EId });
+        }
+    }
+
+    public class A
+    {
+        [Key]
+        public int Id { get; set; }
+        public int BId { get; set; }
+        [Required]
+        public string CId { get; set; }
+        public string DId { get; set; }
+        public string DEId { get; set; }
+        public virtual B B { get; set; }
+        public virtual C C { get; set; }
+        public virtual D D { get; set; }
+    }
+    public class B
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+    public class C
+    {
+        [Key]
+        public string Id { get; set; }
+    }
+    public class D
+    {
+        [Key]
+        public string Id { get; set; }
+        [Key]
+        public string EId { get; set; }
+    }
+    public class E
+    {
+        [Key]
+        public string Id { get; set; }
     }
 }
